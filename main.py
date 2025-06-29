1550209d8227720a692de1f65b0c27e46dab5d8b
vceballosn <vladimir.ceballos@gmail.com>
2025-06-28 22:01:47 -0400

    create update delete api

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from starlette.status import HTTP_200_OK,HTTP_201_CREATED,HTTP_204_NO_CONTENT
+from starlette import status
+
 from model.user_connection import UserConnection
 from schema.user_schema    import UserSchema
 
 app = FastAPI() # <-- This line is crucial!
 conn = UserConnection()
 
-@app.get("/")
+@app.get("/api/python",status_code= HTTP_200_OK)
 async def root():
     items =[]
    ## for data in conn.read_all():
@@ -13,13 +16,28 @@ async def root():
 
     return conn.get_all()
 
-@app.post("/api/insert/")
+@app.post("/api/python/",status_code=HTTP_201_CREATED)
 def insert(user_data:UserSchema):
     data = user_data.dict()
     data.pop("id") # para eliminar el atributo id del formato json 
     print(data)
     conn.write(data)
+    return Response (status_code=HTTP_201_CREATED)
 
-@app.get("/api/user/{id}")
+@app.get("/api/python/{id}",status_code=HTTP_200_OK)
 def get_id(id:str):
     return conn.get_id(id)
+
+@app.delete("/api/python/{id}",status_code=HTTP_204_NO_CONTENT)
+def delete(id:str):
+    print(id)
+    conn.delete(id)
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.put("/api/python/",status_code=HTTP_204_NO_CONTENT)
+def update(user_data:UserSchema):   
+      data = user_data.dict()  
+      print(data)
+      conn.update(data)
+      return Response(status_code=HTTP_204_NO_CONTENT)
--- model/user_connection.py
+++ model/user_connection.py
@@ -33,8 +33,21 @@ class UserConnection():
               INSERT INTO "employees"(first_name,last_name,email_id) VALUES (%(first_name)s,%(last_name)s,%(email_id)s) 
                          """,data)  # interpolacion data es un dicionario en python 
               self.conn.commit()
-    
-    def __def__(self):
+
+    def  update(self,data):  
+          with self.conn.cursor()  as cur:
+               cur.execute("""    
+               UPDATE "employees" SET first_name = %(first_name)s ,last_name = %(last_name)s , email_id=%(email_id)s   WHERE id = %(id)s
+                         """,data)  # interpolacion data es un dicionario en python 
+               self.conn.commit() 
+                   
+
+    def delete(self, id):
+            with self.conn.cursor() as cur:
+                # Pass 'id' as a single-element tuple (id,)
+                cur.execute("""DELETE FROM "employees" WHERE id = %s""", (id,))
+                self.conn.commit()
+    def __def__(self): # descontrutor
         self.conn.close()        
 
    
